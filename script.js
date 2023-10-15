b24feaa12951ad658691725f58973d43eb06a995
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-15 12:45:10 -0500

    added a script function to get browser screen size, updated index, and style-sheet

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Page loaded...");
+
+var screenWidth = window.screen.width;
+var screenHeight = window.screen.height;
+
+function getScreenSize(){
+    console.log(screenWidth + "x" + screenHeight);
+
+}
+
+// run inside browser envirenment 
